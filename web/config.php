9cfefe342c433b5bd335dd32a8722b69f77f2539
aloux00 <aloux@qq.com>
2018-02-04 15:43:07 +0000

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBgwduGCn1328cKaJk_mVnfLQG_tloMS-c');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', 'doubi');//网站名字
+define('TITLENAME', 'doubi');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'aloux@qq.com');//邮箱
 ?>
